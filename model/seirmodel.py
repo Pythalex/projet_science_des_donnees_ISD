040304c03241da483bb6cd7fb5408bb253c3f16a
pythalex <hyorvenn@protonmail.com>
2020-06-18 10:31:39 +0200

    mise au propre de seir dans un seirmodel

---
+++ model/seirmodel.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from scipy.integrate import odeint
+
+class SEIRModel():
+    """
+    Cette classe représente une modélisation SIR épidémiologique.
+    Les paramètres du modèles sont beta et gamma dont les valeurs initiales sont données à
+    l'instanciation de la classe.
+    
+    beta = taux de contact (vitesse de la propagation de la maladie)
+    gamma = moyenne de temps de guérison (vitesse de guérison de la maladie)
+    """
+    def __init__(self, beta_init=0.1, gamma_init=0.3, delta_init=14, R0=0, I0=1, N=67000000, E0=0):
+        self.beta_0 = beta_init
+        self.gamma_0 = gamma_init
+        self.delta_0 = delta_init
+        self.beta_ = None
+        self.gamma_ = None
+        self.delta_ = None
+        # Total population, N.
+        self.N = N
+        # Initial number of infected and recovered individuals, I0 and R0.
+        self.I0, self.R0 = I0, R0
+        # Initial number of exposed (infected not contagious)
+        self.E0 = E0
+        # Everyone else, S0, is susceptible to infection initially.
+        self.S0 = self.N - self.I0 - self.R0 - self.E0
+
+    def fit(self, X, y):
+        """
+        Ajuste les paramètres beta et gamma du modèle sur le jeu de données d'entrée.
+        
+        Returns:
+            self for method chaining.
+        """
+        self.beta_ = self.beta_0
+        self.gamma_ = self.gamma_0
+        self.delta_ = self.delta_0
+        
+        # Initial conditions vector
+        y0 = self.S0, self.E0, self.I0, self.R0
+        
+        f = lambda t, beta, gamma, delta : (odeint(self.deriv, y0, t, args=(self.N, 
+                                                                     beta, 
+                                                                     gamma,
+                                                                     delta)).T)[2] # [2] = only Infected
+        params, covariance = scipy.optimize.curve_fit(f, X, y) # fit
+        self.beta_, self.gamma_, self.delta_ = params
+        
+        print(params)
+        
+        return self
+
+
+    def predict(self, t):
+        """
+        Predict the S I R values for given time
+        TODO : defines values
+        """
+        if self.beta_ is None or self.gamma_ is None:
+            raise Exception("SIRModel not fitted")
+            
+        # Initial conditions vector
+        y0 = self.S0, self.E0, self.I0, self.R0
+        
+        # Integrate the SIR equations over the time grid, t.
+        ret = odeint(self.deriv, y0, t, args=(self.N, self.beta_, self.gamma_, self.delta_))
+        S, E, I, R = ret.T
+            
+        return S, E, I, R
+    
+    
+    def deriv(self, y, t, N, beta, gamma, delta):
+        S, E, I, _ = y
+        dSdt = -beta * S * I / N
+        dEdt = beta * S * I / N - delta * E
+        dIdt = delta * E - gamma * I
+        dRdt = gamma * I
+        return dSdt, dEdt, dIdt, dRdt
--- visual/plot.py
+++ visual/plot.py
@@ -40,3 +40,34 @@ def plot_SIR(S, I, R, t, logy=False):
         ax.spines[spine].set_visible(False)
 
     return ax
+
+
+def plot_SEIR(S, E, I, R, t, logy=False):
+    # Plot the data on three separate curves for S(t), I(t) and R(t)
+    fig = plt.figure(facecolor='w')
+    ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
+
+    if not S is None:
+        ax.plot(t, S, 'b', alpha=0.5, lw=2, label='Susceptible')
+
+    if not E is None:
+        ax.plot(t, E, 'm', alpha=0.5, lw=2, label='Exposed')
+
+    if not I is None:
+        ax.plot(t, I, 'r', alpha=0.5, lw=2, label='Infected')
+
+    if not R is None:
+        ax.plot(t, R, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
+
+    ax.set_xlabel('Time /days')
+    ax.set_ylabel('Number (1000s)')
+
+    ax.yaxis.set_tick_params(length=0)
+    ax.xaxis.set_tick_params(length=0)
+    ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+    legend = ax.legend()
+    legend.get_frame().set_alpha(0.5)
+    for spine in ('top', 'right', 'bottom', 'left'):
+        ax.spines[spine].set_visible(False)
+
+    return ax
